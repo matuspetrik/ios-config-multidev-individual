2aaa4488d69ffbcc40e384759d177f9c37017fe7
mapetrik <matus.petrik@danucem.com>
2025-06-11 17:24:03 +0200

    Added support for Mikrotik

--- Lib/Configurations.py
+++ Lib/Configurations.py
@@ -1,4 +1,4 @@
-
+import sys
 
 def identify_config(file_path):
     with open(file_path, 'r') as file:
@@ -33,3 +33,87 @@ def identify_config(file_path):
         return "mikrotik_routeros"
     else:
         return "Unknown Configuration Type"
+
+def get_common_config(PATH_INPUT, common_config_file="common_config"):
+    # If a special file called `common_config` is present
+    # in the config directory content of this file will be
+    # pushed to the device.
+    with open(PATH_INPUT+common_config_file) as f:
+        lines = f.read().splitlines()
+
+    device_type = identify_config(PATH_INPUT+common_config_file)
+    # Try if `common_config` file exists, if so, notify to be included
+    # in the config
+    common_config = []
+    try:
+        while True:
+            user_input = input(f"NOTE: A special file called `common_config` "
+                               f"of type {device_type} found. Continue? [Y/n]: ")\
+                                   .strip().lower()
+            if 'n' in user_input:
+                print(f"NOTE: Okay , a special file called `common_config` "
+                      "not used. Continuing as usual..")
+                return 0
+            else:
+                return(lines)
+    except:
+        print(f"NOTE: A special file called `common_config` not found. "
+               "Continuing as usual..")
+        return 0
+
+def return_full_config(PATH_INPUT, hostname, common_config):
+    ''' Return device configuration including `common_config`.
+        Input: lines.lst(), common_config.lst()
+        Output: full_config.lst()
+    '''
+    with open(PATH_INPUT+hostname) as f:
+        lines = f.read().splitlines()
+    for line in common_config:
+        lines.append(line)
+    return(lines)
+
+def write_log(filename, input):
+    ''' Write input to filename.
+        Input: filename (full path), input (text to write).
+        Output: A written file.
+    '''
+    with open(filename,'a+') as f:
+        f.write(input+"\r\n")
+    return 1
+
+def get_device_type(each):
+    ''' Get device type statically
+    '''
+    while True:
+        user_input = input(f"Device type determination by config file was "
+                        "UNSUCCESSFUL. Provide manually:\r\n\
+                        [C]isco IOS (prompt for each device)\r\n\
+                        [Ci]sco IOS (no further prompts) \r\n\
+                        [M]ikrotik (prompt for each device)\r\n\
+                        [Mi]krotik (no further prompts)\r\n\
+                        [Q]uit\r\n\
+                        Your choice: "
+                        ).strip().lower()
+        allowed_options = ['c', 'm', 'q', 'ci', 'mi']
+        if user_input in allowed_options:
+            if user_input == "m":
+                return "mikrotik_routeros"
+            if user_input == "mi":
+                each.for_each = False
+                return "mikrotik_routeros"
+            if user_input == "c":
+                return "cisco_ios_ssh"
+            if user_input == "ci":
+                each.for_each = False
+                return "cisco_ios_ssh"
+            if user_input == "q":
+                sys.exit()
+            break
+        else:
+            print(f"Provide valid option.")
+
+
+class ForEach:
+
+    def __init__(self):
+        self.for_each = True
--- Lib/Worker.py
+++ Lib/Worker.py
@@ -1,10 +1,14 @@
 from netmiko import ConnectHandler
 import time
 import datetime
+import Lib.Configurations as Configs
+import sys
 
 def session_worker_cisco_ios(hostname, output_q, router, output_dict, method):
+    ''' A session worker for Cisco IOS devices.
+    '''
    # output_dict = {}
-    with open(PATH_INPUT+hostname) as f:
+    with open(PATH_CONFIG+hostname) as f:
         lines = f.read().splitlines()
     device_session = ConnectHandler(**router)
     time.sleep(2)
@@ -18,10 +22,11 @@ def session_worker_cisco_ios(hostname, output_q, router, output_dict, method):
     #output += device_session.save_config() 
 #    output = net_connect.send_command_timing(command)
     #print(output)
-    if "confirm" in output:
-        output += device_session.send_command_timing("y", strip_prompt=False, strip_command=False) # in case 'y' confirmation is needed
+    if "confirm" in output: # in case 'y' confirmation is needed
+        output += device_session.send_command_timing("y", strip_prompt=False,
+                                                     strip_command=False) 
     output += device_session.save_config()
-    with open(PATH+hostname,'a+') as the_file:
+    with open(PATH_LOG+hostname,'a+') as the_file:
         the_file.write(output)
         the_file.write("\r\n")
     output_dict[hostname+".log"]  = output
@@ -29,29 +34,78 @@ def session_worker_cisco_ios(hostname, output_q, router, output_dict, method):
     time.sleep(2)
     device_session.disconnect()
 
-def session_worker_mikrotik(hostname, output_q, router, output_dict, method, PATH_INPUT, PATH):
-   # output_dict = {}
-    print(PATH_INPUT)
-    with open(PATH_INPUT+hostname) as f:
-        lines = f.read().splitlines()
-    print(lines)
+def session_worker_mikrotik(
+                            hostname,
+                            output_q,
+                            router,
+                            output_dict,
+                            common_config,
+                            method,
+                            PATH_CONFIG,
+                            PATH_LOG,
+                            ):
+    ''' A session worker for Mikrotik OS devices.
+    '''
+    lines = Configs.return_full_config(PATH_CONFIG, hostname, common_config)
     device_session = ConnectHandler(**router)
-    print(lines)
-    time.sleep(2)
+    # print(device_session)
     output_dict[hostname+"_"+method] = ""
     datetime_then = datetime.datetime.now()
-    output = f"##############################################\r\n# {hostname} config started at: "+str(datetime_then)+" #\r\n||||||||||||||||||||||||||||||||||||||||||||||\r\n"
+    output = f"### {hostname} config started at: { str(datetime_then) } ###\r\n\r\n"
     for line in lines:
-        output += device_session.send_command(line)
-    if "confirm" in output:
-        output += device_session.send_command_timing("y", strip_prompt=False, strip_command=False) # in case 'y' confirmation is needed
-    # output += device_session.save_config()
-    output += f"||||||||||||||||||||||||||||||||||||||||||||||\r\n# {hostname} config completed in "+str((datetime.datetime.now()-datetime_then).total_seconds())+" seconds. #\r\n##############################################\r\n"
-    print(output)
-    with open(PATH+hostname,'a+') as the_file:
-        the_file.write(output)
-        the_file.write("\r\n")
+        status = "OK"
+        try:
+            tmp_output = device_session.send_command(line)
+            if "failure:" in tmp_output.strip().lower():
+                status = "FAIL"
+                output += f"{ status } ---> { line }\r\n"
+                output += f"{ tmp_output }\r\n"
+            else:
+                output += f"{ status } ---> { line }\r\n"
+            if "confirm" in output: # in case 'y' confirmation is needed
+                output += device_session.send_command_timing(
+                    "y",
+                    strip_prompt=False,
+                    strip_command=False
+                    ) 
+        except Exception as e:
+            status = "FAIL"
+            output += f"{ status } -> { line }, { e }\r\n"
+    output += f"\r\n### {hostname} config completed in "+str((datetime.datetime.now()\
+        -datetime_then).total_seconds())+" seconds. ###\r\n"
+    Configs.write_log(PATH_LOG+hostname, output)
+    # print(output)
     output_dict[hostname+".log"]  = output
+    # output_dict[hostname]  = output
     output_q.put(output_dict)
-    time.sleep(2)
     device_session.disconnect()
+    time.sleep(2)
+
+def session_worker_general(**kwargs):
+    if 'mikrotik' in kwargs.get('device_type'):
+        session_worker_mikrotik(
+            kwargs.get('hostname'),
+            kwargs.get('output_q'),
+            kwargs.get('router'),
+            kwargs.get('output_dict'),
+            kwargs.get('common_config'),
+            kwargs.get('method'),
+            kwargs.get('PATH_CONFIG'),
+            kwargs.get('PATH_LOG'),
+        )
+    elif 'cisco_ios' in kwargs.get('device_type'):
+        session_worker_cisco_ios(
+            # hostname,
+            # output_q,
+            # router,
+            # output_dict,
+            # method="ssh"
+            kwargs.get('hostname'),
+            kwargs.get('output_q'),
+            kwargs.get('router'),
+            kwargs.get('output_dict'),
+            kwargs.get('method'),
+            kwargs.get('common_config'),
+            kwargs.get('PATH_CONFIG'),
+            kwargs.get('PATH_LOG')
+        )
